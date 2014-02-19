9db10d011e58847f17b9a952ceeaf8ffdec4f772
lafourit <dadycitroen@gmail.com>
2014-02-19 23:05:39 +0000

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]){
 	printf("~~~~~~~~~~~~~~~~~~\n");
-	printf("~~Hello World!\n~~");
+	printf("~~Hello World~~~~~\n");
 	printf("~~~~~~~~~~~~~~~~~~\n");
 	return 0;
 }
